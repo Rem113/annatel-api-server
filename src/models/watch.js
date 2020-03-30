83e5460621e0c969671ae5a5355b9c1f1f99b501
Hillel Saal <hillelsaal@kalstudies.com>
2020-03-30 12:00:53 +0200

    Updated Watch model to contain a field

--- src/models/watch.js
+++ src/models/watch.js
@@ -1,5 +1,10 @@
 import mongoose, { Schema } from "mongoose";
 
-const Watch = new Schema({});
+const Watch = new Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+    required: true
+  }
+});
 
 export default mongoose.model("Watch", Watch, "Watches");
