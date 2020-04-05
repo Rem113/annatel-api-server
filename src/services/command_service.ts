ff31c31876e6fcffb935a1afbcfbc26d9fd819d5
Hillel Saal <remi.saal@gmail.com>
2020-04-05 15:35:53 +0200

    Fixed annatel-watch-parser type problems

--- src/services/command_service.ts
+++ src/services/command_service.ts
@@ -1,59 +1,59 @@
-// import { serialize } from "annatel-watch-parser";
+import { serialize } from "annatel-watch-parser";
 
 import CommandRepository from "../repositories/command_repository";
 import WatchRepository from "../repositories/watch_repository";
 
 // TODO: Document
-// const validateParams = (actionsType, params) => {
-//   const objParams = new Object(params);
+const validateParams = (actionType: any, params: any) => {
+  const objParams = new Object(params);
 
-//   switch (actionType) {
-//     case "UPLOAD":
-//       return objParams.hasOwnProperty("interval");
-//     case "CENTER":
-//       return objParams.hasOwnProperty("centerNumber");
-//     case "SLAVE":
-//       return objParams.hasOwnProperty("assistanceNumber");
-//     case "PW":
-//       return objParams.hasOwnProperty("password");
-//     case "CALL":
-//       return objParams.hasOwnProperty("phoneNumber");
-//     case "SMS":
-//       return (
-//         objParams.hasOwnProperty("phoneNumber") &&
-//         objParams.hasOwnProperty("message")
-//       );
-//     case "SOS":
-//     case "SOS1":
-//     case "SOS2":
-//     case "SOS3":
-//       return objParams.hasOwnProperty("sosNumber");
-//     default:
-//       return true;
-//   }
-// };
+  switch (actionType) {
+    case "UPLOAD":
+      return objParams.hasOwnProperty("interval");
+    case "CENTER":
+      return objParams.hasOwnProperty("centerNumber");
+    case "SLAVE":
+      return objParams.hasOwnProperty("assistanceNumber");
+    case "PW":
+      return objParams.hasOwnProperty("password");
+    case "CALL":
+      return objParams.hasOwnProperty("phoneNumber");
+    case "SMS":
+      return (
+        objParams.hasOwnProperty("phoneNumber") &&
+        objParams.hasOwnProperty("message")
+      );
+    case "SOS":
+    case "SOS1":
+    case "SOS2":
+    case "SOS3":
+      return objParams.hasOwnProperty("sosNumber");
+    default:
+      return true;
+  }
+};
 
-// const mapActionToObject = (watch, actionType, params) => {
-//   const isValid = validateParams();
+const mapActionToObject = (watch: any, actionType: any, params: any) => {
+  const isValid = validateParams(actionType, params);
 
-//   const temp = {
-//     vendor: watch.vendor,
-//     watchId: watch.watchId,
-//     actionType,
-//     payload: () => {
-//       switch (actionType) {
-//         case "UPLOAD":
-//           return { interval: params.interval };
-//         case "CENTER":
-//           return { centerNumber: params.centerNumber };
-//         case "SLAVE":
-//           return { assistanceNumber: params.assistanceNumber };
-//         case "PW":
-//           return { password: params.password };
-//       }
-//     },
-//   };
-// };
+  const temp = {
+    vendor: watch.vendor,
+    watchId: watch.watchId,
+    actionType,
+    payload: () => {
+      switch (actionType) {
+        case "UPLOAD":
+          return { interval: params.interval };
+        case "CENTER":
+          return { centerNumber: params.centerNumber };
+        case "SLAVE":
+          return { assistanceNumber: params.assistanceNumber };
+        case "PW":
+          return { password: params.password };
+      }
+    },
+  };
+};
 
 export default class CommandService {
   commandRepository: CommandRepository;
---
+++ src/types/annatel_watch_parser.d.ts
@@ -0,0 +1,1 @@
+declare module "annatel-watch-parser";
